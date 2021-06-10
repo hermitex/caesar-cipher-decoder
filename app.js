eb8a584a8fbb1c486a9e1a46f1020af6ab9dcffb
Tiberius Mairura <tiberiusmairura90@gmail.com>
2021-06-11 00:27:06 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const inputText = document.getElementById('input');
+const buttonOption = document.querySelectorAll('button');
+const levelInput = document.getElementById('level');
+const outputWindow = document.getElementById('output');
+
+const encryptText = (input, level) => {
+  let letter = '';
+  let inputArray = input.split('');
+  inputArray.forEach((char) => {
+    letter += String.fromCharCode(char.charCodeAt(0) + level);
+  });
+  outputWindow.textContent = letter;
+  level = 0;
+};
+const decryptText = (input, level) => {
+  let letter = '';
+  let inputArray = input.split('');
+  inputArray.forEach((char) => {
+    letter += String.fromCharCode(char.charCodeAt(0) - level);
+  });
+  outputWindow.textContent = letter;
+  level = 1;
+};
+
+const getOptipon = (e) => {
+  let input = inputText.value;
+  let action = e.target.id;
+  let level = parseInt(levelInput.value);
+  if (input != ' ') {
+    if (action === 'encrypt') encryptText(input, level);
+    else decryptText(input, level);
+  } else {
+    confirm('Error! Empty input!');
+    return;
+  }
+};
+
+buttonOption.forEach((button) => (button.onclick = getOptipon));
