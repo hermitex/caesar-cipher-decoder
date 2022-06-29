43d22c12ce6d16f0efe0d7b53aedbb78b2ed68a9
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-06-29 18:20:10 +0300

    add js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const inputText = document.getElementById('input');
-const buttonOption = document.querySelectorAll('button');
-const levelInput = document.getElementById('level');
-const outputWindow = document.getElementById('output');
+const inputText = document.getElementById("input");
+const buttonOption = document.querySelectorAll("button");
+const levelInput = document.getElementById("level");
+const outputWindow = document.getElementById("output");
 
 const encryptText = (input, level) => {
-  let letter = '';
-  let inputArray = input.split('');
+  let letter = "";
+  let inputArray = input.split("");
   inputArray.forEach((char) => {
     letter += String.fromCharCode(char.charCodeAt(0) + level);
   });
@@ -13,8 +13,8 @@ const encryptText = (input, level) => {
   level = 0;
 };
 const decryptText = (input, level) => {
-  let letter = '';
-  let inputArray = input.split('');
+  let letter = "";
+  let inputArray = input.split("");
   inputArray.forEach((char) => {
     letter += String.fromCharCode(char.charCodeAt(0) - level);
   });
@@ -23,14 +23,15 @@ const decryptText = (input, level) => {
 };
 
 const getOptipon = (e) => {
+  e.preventDefault();
   let input = inputText.value;
   let action = e.target.id;
   let level = parseInt(levelInput.value);
-  if (input != ' ') {
-    if (action === 'encrypt') encryptText(input, level);
+  if (input != " ") {
+    if (action === "encrypt") encryptText(input, level);
     else decryptText(input, level);
   } else {
-    confirm('Error! Empty input!');
+    confirm("Error! Empty input!");
     return;
   }
 };
